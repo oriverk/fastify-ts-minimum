5bd90bf8d1dec4c627b2e4e2d10976e640ee782f
oriverk <oriveryk@gmail.com>
2021-03-09 17:48:16 +0900

    modify index.ts

--- api/index.ts
+++ api/index.ts
@@ -4,13 +4,28 @@ interface Query {
   name?: string;
 }
 
-const app = fastify.fastify({ logger: true });
+const app = fastify.fastify({
+  logger: true,
+  ignoreTrailingSlash: true
+});
 
 app.get<{ Querystring: Query }>("/", async (req, res) => {
   const { name = "World" } = req.query;
   req.log.info({ name }, "hello world!");
   return `Hello ${name}!`;
 });
+
+app.get('/cards', async (_req, _res) => {
+  return { hello: 'world' }
+})
+
+app.get('/skills', async (_req, _res) => {
+  return { page: 'skills' }
+})
+
+app.get('/unit-skills', async (_req, res) => {
+  return { page: 'unit-skills' }
+})
 
 const handler = async (req: fastify.FastifyRequest, res: fastify.FastifyReply) => {
   await app.ready();
