c6731317046db656b71a1cfa35b6d2de9b8bff80
oriverk <oriveryk@gmail.com>
2021-03-10 18:55:41 +0900

    refer prisma-heroku example

--- api/index.ts
+++ api/index.ts
@@ -9,27 +9,39 @@ const app = fastify.fastify({
   ignoreTrailingSlash: true
 });
 
-app.get<{ Querystring: Query }>("/", async (req, res) => {
+app.get<{ Querystring: Query }>("/api", async (req, _res) => {
   const { name = "World" } = req.query;
   req.log.info({ name }, "hello world!");
   return `Hello ${name}!`;
 });
 
-app.get('/cards', async (_req, _res) => {
+app.get('/api/cards', async (_req, _res) => {
   return { hello: 'world' }
 })
 
-app.get('/skills', async (_req, _res) => {
+app.get('/api/skills', async (_req, _res) => {
   return { page: 'skills' }
 })
 
-app.get('/unit-skills', async (_req, res) => {
+app.get('/api/unit-skills', async (_req, _res) => {
   return { page: 'unit-skills' }
 })
 
-const handler = async (req: fastify.FastifyRequest, res: fastify.FastifyReply) => {
-  await app.ready();
-  app.server.emit("request", req, res);
-};
+const PORT = process.env.PORT || 3000
+const start = async () => {
+  try {
+    await app.listen(PORT)
+  } catch (error) {
+    app.log.error(error)
+    process.exit(1)
+  }
+}
 
-export default handler;
+export default start()
+
+// const handler = async (req: fastify.FastifyRequest, res: fastify.FastifyReply) => {
+//   await app.ready();
+//   app.server.emit("request", req, res);
+// };
+
+// export default handler;
